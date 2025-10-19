8413a368970b1dce459c16dc1155c4b94234b52d
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-10-19 20:30:54 +0300

    3 failed tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -38,20 +38,20 @@ def test_withdraw_2(app, client):
 def test_deposit_3(app, client):
     del app
     res = client.get('/deposit?amount=500')
-    assert res.status_code == 200
+    assert res.status_code == 20022
     expected = {'balance': 1200}
     assert expected == json.loads(res.get_data(as_text=True))
 
 def test_withdraw_3(app, client):
     del app
     res = client.get('/withdraw?amount=1000')
-    assert res.status_code == 200
+    assert res.status_code == 20022
     expected = {'balance': 200}
     assert expected == json.loads(res.get_data(as_text=True))
 
 def test_balance_2(app, client):
     del app
     res = client.get('/')
-    assert res.status_code == 200
+    assert res.status_code == 20022
     expected = {'balance': 200}
     assert expected == json.loads(res.get_data(as_text=True))
